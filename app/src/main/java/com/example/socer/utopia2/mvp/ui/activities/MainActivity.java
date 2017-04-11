3ae2f7faca9f180dbbd67932b63a34fa2ec2ff7b
clmiberf <clmiberf@gmail.com>
2017-04-11 19:46:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/socer/utopia2/mvp/ui/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.socer.utopia2.mvp.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.socer.utopia2.mvp.ui.activities.base.BaseActivity;
+
+/**
+ * Created by socer on 17-4-10.
+ */
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
---
+++ app/src/main/java/com/example/socer/utopia2/mvp/ui/activities/base/BaseActivity.java
@@ -0,0 +1,31 @@
+package com.example.socer.utopia2.mvp.ui.activities.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by socer on 17-4-10.
+ */
+
+public abstract  class BaseActivity extends AppCompatActivity {
+
+    public abstract int getLayoutId();
+
+    protected abstract void initVariables();
+
+    //先初始化配置，再初始化布局
+    protected abstract void initViews();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int layoutId = getLayoutId();
+        setContentView(layoutId);
+        ButterKnife.bind(this);
+        initVariables();
+        initViews();
+    }
+}
