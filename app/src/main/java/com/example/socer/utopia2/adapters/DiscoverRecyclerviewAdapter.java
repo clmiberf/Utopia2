934f821a9f15dfca388e807829f56b1c37304d73
clmiberf <clmiberf@gmail.com>
2017-04-18 21:08:27 +0800

    添加了发现功能模块的几个view,写了几个presenter,写了discover界面的item和adapter

--- app/src/main/java/com/example/socer/utopia2/adapters/DiscoverRecyclerviewAdapter.java
+++ app/src/main/java/com/example/socer/utopia2/adapters/DiscoverRecyclerviewAdapter.java
@@ -6,7 +6,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.socer.utopia2.R;
 import com.example.socer.utopia2.mvp.model.beans.DisccoverMsgModelBean;
 
@@ -18,20 +24,36 @@ import java.util.List;
 
 public class DiscoverRecyclerviewAdapter extends RecyclerView.Adapter<DiscoverRecyclerviewAdapter.DiscoverViewHolder> {
 
-    private Context mCOntext;
+    private Context mContext;
 
     protected List<DisccoverMsgModelBean> items;
 
 
     @Override
     public DiscoverViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mCOntext).inflate(R.layout.fragment_discover_items_layout,parent,false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fragment_discover_items_layout,parent,false);
         return new DiscoverViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(DiscoverViewHolder holder, int position) {
+        DisccoverMsgModelBean msgModelBean = items.get(position);
 
+        Glide.with(mContext)
+                .load(msgModelBean.getPublisherPictrueUrl().toString())
+                .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                .into(holder.imageView);
+        holder.publisher.setText(msgModelBean.getPublisher().toString());
+        holder.contentView.setText(msgModelBean.getTopicContent().toString());
+        holder.lightTextView.setText(msgModelBean.getLightenSum());
+        holder.commentTextView.setText(msgModelBean.getCommentSum());
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,"平哥最帅",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -41,9 +63,21 @@ public class DiscoverRecyclerviewAdapter extends RecyclerView.Adapter<DiscoverRe
     }
 
     public class DiscoverViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout linearLayout;
+        ImageView imageView;
+        TextView publisher;
+        TextView lightTextView;
+        TextView commentTextView;
+        TextView contentView;
 
         public DiscoverViewHolder(View itemView) {
             super(itemView);
+            linearLayout = (LinearLayout) itemView.findViewById(R.id.diacover_item_linearlayout);
+            imageView = (ImageView) itemView.findViewById(R.id.discover_item_user_head_pictrue);
+            lightTextView = (TextView) itemView.findViewById(R.id.discover_item_light_number);
+            commentTextView = (TextView) itemView.findViewById(R.id.discover_item_comment_number);
+            publisher = (TextView) itemView.findViewById(R.id.discover_user_nickname);
+            contentView = (TextView) itemView.findViewById(R.id.discover_item_content);
         }
     }
 
